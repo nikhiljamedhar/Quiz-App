646fb3ce1eef217bd3cfb7a42024701a2484fc42
vinoth <vinothr@thoughtworks.com>
2015-09-28 18:31:16 +0530

    Vinoth / Nikhil - Moved application to live git repo

---
+++ app/js/app.js
@@ -0,0 +1,37 @@
+var pencilBoxApp = angular.module('pencilBoxApp',['ngResource', 'ngRoute']);
+
+pencilBoxApp.config(['$routeProvider',function($routeProvider){
+  "use strict";
+
+  $routeProvider.when('/grades',{
+    templateUrl : 'partials/home-view.html',
+    controller: 'GradeListController'
+  })
+  .when('/grades/:gradeId', {
+    templateUrl: 'partials/subjects-view.html',
+    controller: 'SubjectListController'
+  })
+  .when('/grades/:gradeId/subject/:subjectId/',{
+    templateUrl: 'partials/chapters-view.html',
+    controller: 'ChapterListController'
+  })
+  .when('/grades/:gradeId/subject/:subjectId/:chapterId/',{
+    templateUrl:'partials/contents-view.html',
+    controller:'ContentListController'
+  })
+  .when('/grades/:gradeId/subject/:subjectId/:videoId/',{
+    templateUrl:'partials/contents-view.html',
+    controller:'ContentListController'
+  })
+  // .when('/otherApps', {
+  //   templateUrl : 'partials/other-apps.html',
+  //   controller : 'OtherAppController'
+  // })
+  .when('/search/:keyword', {
+    templateUrl: 'partials/search-results-view.html',
+    controller: 'SearchResultController'
+  })
+  .otherwise({
+    redirectTo: '/grades'
+  });
+}]);
---
+++ app/js/services.js
@@ -0,0 +1,33 @@
+
+"use strict";
+
+pencilBoxApp.factory('Grades', ['$resource',
+  function($resource){
+    return $resource('json/grades.json', {}, {
+      query: {method:'GET', isArray:true }
+    });
+  }]);
+pencilBoxApp.factory('Subjects', ['$resource',
+  function($resource){
+    return $resource('json/:gradeId/subjects.json', {}, {
+      query: {method:'GET', isArray:true }
+    });
+  }]);
+pencilBoxApp.factory('Chapters', ['$resource',
+    function($resource){
+      return $resource('json/:gradeId/:subjectId/chapters.json', {}, {
+        query: {method:'GET', isArray:true }
+    });
+  }]);
+pencilBoxApp.factory('Contents', ['$resource',
+    function($resource){
+      return $resource('json/:gradeId/:subjectId/:chapterId.json', {}, {
+        query: {method: 'GET', isArray:true }
+      });
+    }]);
+pencilBoxApp.factory('Apps', ['$resource',
+      function($resource){
+        return $resource('json/all.json', {}, {
+          query: {method:'GET', isArray:true }
+        });
+      }]);
