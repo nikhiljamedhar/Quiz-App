2402937140faa5ebd6f0fb9ab562cc23fec2dc9b
Abdulsattar Mohammed <asattar.md@gmail.com>
2015-11-03 17:01:54 +0530

    Nikhil/Sattar fix MF change the type

--- app/js/quiz-controller.js
+++ app/js/quiz-controller.js
@@ -346,10 +346,11 @@ pencilBoxApp.controller('CreateQuizController', ['$scope', '$routeParams', 'Crea
             var setType = function () {
                 $scope.currentQuestion = $scope.quizJson.questions[$scope.currentQuestionIndex] = getDefaultQuestion(questionType);
             };
-            if ($scope.currentQuestion.question) {
+            if (($scope.currentQuestion.question && $scope.currentQuestion.question.trim())
+                    || ($scope.currentQuestion.questions && $scope.currentQuestion.questions.filter(function(q) {return q.question || q.answer;}).length > 0)) {
                 new CustomDialog($q, {
                     title: "Confirm",
-                    description: "You have entered some question details. If you change the type, they'll be lost. Are you sure, you want to change the type?",
+                    description: "You have entered question details. If you change the type, they'll be lost. Do you want to continue?",
                     buttons: ["ok", "cancel"]
                 }).show().then(setType);
             } else {
